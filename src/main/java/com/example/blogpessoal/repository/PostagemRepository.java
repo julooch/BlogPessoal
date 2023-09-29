5262040ce80a13117c21292a035bc41c829b4ce9
julooch <juliadomingues.br@gmail.com>
2023-09-29 12:46:36 -0300

    Primeiro commit

---
+++ src/main/java/com/example/blogpessoal/repository/PostagemRepository.java
@@ -0,0 +1,9 @@
+package com.example.blogpessoal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.blogpessoal.model.Postagem;
+
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+
+}
